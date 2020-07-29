71289631d82bccec20f0f0392cabafdd98c1cbec
Konstantin Ostrovskiy <grinv@outlook.com>
2020-07-30 00:07:49 +0300

    Next gen

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,31 @@
-define(['jquery'], function($) {
-  return $("#preload-box i").delay(10).fadeIn(2000);
-});
+import $ from 'jquery';
+import 'fullpage.js/dist/jquery.fullPage.js';
+// import 'fullpage.js';
+// export for others scripts to use
+// window.$ = $;
+// window.jQuery = jQuery;
 
-define(['jquery', 'fullpage.js'], function($, fullpage) {
-  return $('#fullpage').fullpage({
-    anchors: ['main', 'about', 'portfolio', 'contacts'],
-    menu: "#menu",
-    loopBottom: true,
-    afterLoad: function(link, index) {
-      if ($(".section.active .wrapper").hasClass('novisible') && index !== 1) {
-        $(".section.active .wrapper").delay(2000).removeClass("novisible").addClass('visible animate__animated animate__fadeIn');
-        $(".section.active .scrollDown").fadeIn(2000);
-        if (index === 4) {
-          return $("#links").removeClass("novisible").addClass('visible animate__animated animate__fadeIn');
-        }
+$("#preload-box i").delay(10).fadeIn(2000);
+$('#fullpage').fullpage({
+  anchors: ['main', 'about', 'portfolio', 'contacts'],
+  menu: "#menu",
+  loopBottom: true,
+  afterLoad: function(link, index) {
+    if ($(".section.active .wrapper").hasClass('novisible') && index !== 1) {
+      $(".section.active .wrapper").delay(2000).removeClass("novisible").addClass('visible animate__animated animate__fadeIn');
+      $(".section.active .scrollDown").fadeIn(2000);
+      if (index === 4) {
+        return $("#links").removeClass("novisible").addClass('visible animate__animated animate__fadeIn');
       }
-    },
-    afterRender: function() {
-      $(".wrapper, #links").addClass('novisible');
-      $(".section.active .wrapper, .section.active .scrollDown").fadeOut();
-      return $(window).ready(function() {
-        $("#preload-box").delay(1000).fadeOut('slow');
-        $(".section.active .wrapper").delay(1500).removeClass("novisible").fadeIn(3000);
-        return $(".section.active .scrollDown").delay(1000).fadeIn(6000);
-      });
     }
-  });
+  },
+  afterRender: function() {
+    $(".wrapper, #links").addClass('novisible');
+    $(".section.active .wrapper, .section.active .scrollDown").fadeOut();
+    return $(window).ready(function() {
+      $("#preload-box").delay(1000).fadeOut('slow');
+      $(".section.active .wrapper").delay(1500).removeClass("novisible").fadeIn(3000);
+      return $(".section.active .scrollDown").delay(1000).fadeIn(6000);
+    });
+  }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,100 @@
-const webpack = require("webpack");
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const extractCSS = new ExtractTextPlugin('../build/[name].css');
+const path = require('path');
 
 module.exports = {
-  entry: "./src/entry.js",
+  entry: './src/entry.js',
   output: {
-    path: __dirname + "/build",
-    filename: "main.js"
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: "./build/"
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.css$/,
-        loader: extractCSS.extract("style-loader", "css-loader")
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       }, {
-        test: /\.scss$/i,
-        loader: extractCSS.extract(['css', 'sass'])
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+          }
+        ]
       }, {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url?limit=10000&minetype=application/font-woff"
-      }, {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/octet-stream"
-      }, {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file"
-      }, {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=image/svg+xml"
-      }, {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loader: 'url?limit=10000!img?progressive=true'
-      }, {
-        test: /\.coffee$/,
-        loader: "coffee-loader"
-      }, {
-        test: /\.(coffee\.md|litcoffee)$/,
-        loader: "coffee-loader?literate"
-      }
-    ]
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            }
+          },
+        ],
+      },
+    ],
   },
-  amd: {
-    jQuery: true
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }), new webpack.ProvidePlugin({
-      "$": "jquery",
-      "jquery": "jQuery",
-      "windows.jQuery": "jquery"
-    }), extractCSS
-  ]
 };
+
+// const webpack = require("webpack");
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const extractCSS = new ExtractTextPlugin('../build/[name].css');
+
+// module.exports = {
+//   entry: "./src/entry.js",
+//   output: {
+//     path: __dirname + "/build",
+//     filename: "main.js"
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.css$/,
+//         loader: extractCSS.extract("style-loader", "css-loader")
+//       }, {
+//         test: /\.scss$/i,
+//         loader: extractCSS.extract(['css', 'sass'])
+//       }, {
+//         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+//         loader: "url?limit=10000&minetype=application/font-woff"
+//       }, {
+//         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+//         loader: "url?limit=10000&mimetype=application/octet-stream"
+//       }, {
+//         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+//         loader: "file"
+//       }, {
+//         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+//         loader: "url?limit=10000&mimetype=image/svg+xml"
+//       }, {
+//         test: /\.(jpe?g|png|gif|svg)$/i,
+//         loader: 'url?limit=10000!img?progressive=true'
+//       }, {
+//         test: /\.coffee$/,
+//         loader: "coffee-loader"
+//       }, {
+//         test: /\.(coffee\.md|litcoffee)$/,
+//         loader: "coffee-loader?literate"
+//       }
+//     ]
+//   },
+//   amd: {
+//     jQuery: true
+//   },
+//   plugins: [
+//     new webpack.optimize.UglifyJsPlugin({
+//       compress: {
+//         warnings: false
+//       }
+//     }), new webpack.ProvidePlugin({
+//       "$": "jquery",
+//       "jquery": "jQuery",
+//       "windows.jQuery": "jquery"
+//     }), extractCSS
+//   ]
+// };
