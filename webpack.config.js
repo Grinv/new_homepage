326a41c37f9558ec2c3f8e5b9cb9476637a3e788
Konstantin Ostrovskiy <grinv@outlook.com>
2020-07-30 11:18:42 +0300

    Fixes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default
 
-module.exports = {
+module.exports = (env, argv) => ({
   entry: './src/entry.js',
   output: {
     filename: 'main.js',
@@ -15,7 +15,10 @@ module.exports = {
         { from: './src/img/images/', to: 'img/' },
       ],
     }),
-    new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i })
+    new ImageminPlugin({
+      disable: argv.mode !== 'production',
+      test: /\.(jpe?g|png|gif|svg)$/i,
+    })
   ],
   module: {
     rules: [
@@ -49,62 +52,4 @@ module.exports = {
       },
     ],
   },
-};
-
-// const webpack = require("webpack");
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// const extractCSS = new ExtractTextPlugin('../build/[name].css');
-
-// module.exports = {
-//   entry: "./src/entry.js",
-//   output: {
-//     path: __dirname + "/build",
-//     filename: "main.js"
-//   },
-//   module: {
-//     loaders: [
-//       {
-//         test: /\.css$/,
-//         loader: extractCSS.extract("style-loader", "css-loader")
-//       }, {
-//         test: /\.scss$/i,
-//         loader: extractCSS.extract(['css', 'sass'])
-//       }, {
-//         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-//         loader: "url?limit=10000&minetype=application/font-woff"
-//       }, {
-//         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-//         loader: "url?limit=10000&mimetype=application/octet-stream"
-//       }, {
-//         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-//         loader: "file"
-//       }, {
-//         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-//         loader: "url?limit=10000&mimetype=image/svg+xml"
-//       }, {
-//         test: /\.(jpe?g|png|gif|svg)$/i,
-//         loader: 'url?limit=10000!img?progressive=true'
-//       }, {
-//         test: /\.coffee$/,
-//         loader: "coffee-loader"
-//       }, {
-//         test: /\.(coffee\.md|litcoffee)$/,
-//         loader: "coffee-loader?literate"
-//       }
-//     ]
-//   },
-//   amd: {
-//     jQuery: true
-//   },
-//   plugins: [
-//     new webpack.optimize.UglifyJsPlugin({
-//       compress: {
-//         warnings: false
-//       }
-//     }), new webpack.ProvidePlugin({
-//       "$": "jquery",
-//       "jquery": "jQuery",
-//       "windows.jQuery": "jquery"
-//     }), extractCSS
-//   ]
-// };
+});
